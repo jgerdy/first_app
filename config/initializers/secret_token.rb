bb3bfc8bb674300317da3d4c942f7065345a83fe
jgerdy <jim@cheeseplanet.org>
2010-09-06 10:38:28 -0500

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '97ac82f0e06074511da129f3c729e28a2acc827f75642f358fc2c951720bf4897bba1136d382c98d6f5068189289e92952a94b9328f2b73eec2ddc0449d3baaf'
